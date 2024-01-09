a54c1707b2be6c3c6a9620217b9eaa8998ac1c1d
grigoris.bachtsevanos <grigoris.bachtsevanos@weare8.com>
2024-01-09 15:42:42 +0200

    changes

---
+++ .storybook/theme.ts
@@ -0,0 +1,8 @@
+import { create } from "@storybook/theming/create";
+
+export default create({
+  base: "dark",
+  brandTitle: "Eight.WebComponent.Library",
+  brandImage: "/logo.png",
+  brandTarget: "_self",
+});
